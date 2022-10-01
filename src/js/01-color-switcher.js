912d4d7aaaa1101774fae63130bd5028a203f4bc
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-10-01 11:13:49 +0300

    Upd 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function changeColor() {
+  setTimeout(getRandomHexColor(), 1000);
+}
+
+function stopChangeColor() {}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChangeColor);
