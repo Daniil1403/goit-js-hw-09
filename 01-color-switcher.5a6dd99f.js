db63cb0c7a7b6d77733b3f5f5d2962ef0fdcd927
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-10-01 08:14:13 +0000

    Deploying to gh-pages from @ Daniil1403/goit-js-hw-09@912d4d7aaaa1101774fae63130bd5028a203f4bc 🚀

---
+++ 01-color-switcher.5a6dd99f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){setTimeout("#".concat(Math.floor(16777215*Math.random()).toString(16)),1e3)})),e.addEventListener("click",(function(){}))}();
+//# sourceMappingURL=01-color-switcher.5a6dd99f.js.map
