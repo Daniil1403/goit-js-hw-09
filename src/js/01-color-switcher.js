ab02da1750d225315618e4698ab36be346aa39cd
Daniil1403 <104705712+Daniil1403@users.noreply.github.com>
2022-10-05 19:30:06 +0300

    add task 1-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,28 @@
-const startBtn = document.querySelector('button[data-start]');
-const stopBtn = document.querySelector('button[data-stop]');
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let changeId = null;
 
-function changeColor() {
-  setTimeout(getRandomHexColor(), 1000);
+refs.startBtn.addEventListener('click', startChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  setBodyColor();
+  changeId = setInterval(setBodyColor, 1000);
 }
 
-function stopChangeColor() {}
+function stopChangeColor() {
+  clearInterval(changeId);
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function setBodyColor() {
+  refs.body.style.background = getRandomHexColor();
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-startBtn.addEventListener('click', changeColor);
-stopBtn.addEventListener('click', stopChangeColor);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+  submitButton: document.querySelector('button'),
+};
+let intervalId = null;
+
+refs.submitButton.addEventListener('click', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const delay = +refs.delay.value;
+  const step = +refs.step.value;
+  const amount = +refs.amount.value;
+  callPromise(delay, step, amount);
+}
+
+function callPromise(delay, step, amount) {
+  setTimeout(() => {
+    let position = 0;
+    intervalId = setInterval(() => {
+      position += 1;
+      if (position === +amount) {
+        clearInterval(intervalId);
+      }
+      createPromise(position, delay);
+      delay += step;
+    }, step);
+  }, delay);
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise(() => {
+    const shouldResolve = Math.random() > 0.3;
+
+    if (shouldResolve) {
+      // Fulfill
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      // Reject
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    }
+  });
 }
